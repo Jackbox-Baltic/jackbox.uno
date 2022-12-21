3175b2ac77ea0ac483a21d0cffe6f270ec464b2c
dedz212 <dejelniekskonta@gmail.com>
2022-12-21 22:31:43 +0200

    221221
    + Updated .html files

--- assets/js/language.js
+++ assets/js/language.js
@@ -94,6 +94,19 @@ const gamesDatatest= [
   }
 ];
 
+const newgamesDatatest= [
+  {
+    title: "Recently translated game",
+    href: "tjpp4",
+    image: "TeeKOGame",
+    name: "Tee K.O.",
+    span: "THE JACKBOX PARTY PACK 4",
+    date: "December 21, 2022",
+    new: "<p data-v-556a942f=\"\" class=\"new\">NEW</p>",
+    view: "<p data-v-556a942f=\"\" class=\"cta\">VIEW THIS GAME <svg data-v-556a942f=\"\" class=\"external-icon\" viewBox=\"0 0 200 200\"><path d=\"M185,106c-8.3,0-15,6.7-15,15v44c0,2.8-2.2,5-5,5H35c-2.8,0-5-2.2-5-5V35c0-2.8,2.2-5,5-5h44c8.3,0,15-6.7,15-15S87.3,0,79,0H35C15.7,0,0,15.7,0,35v130c0,19.3,15.7,35,35,35h130c19.3,0,35-15.7,35-35v-44C200,112.7,193.3,106,185,106z\"></path><path d=\"M184,0h-54c-8.3,0-15,6.7-15,15s6.7,15,15,15h18.8L90.4,88.4c-5.9,5.9-5.9,15.4,0,21.2c2.9,2.9,6.8,4.4,10.6,4.4s7.7-1.5,10.6-4.4L170,51.2V70c0,8.3,6.7,15,15,15s15-6.7,15-15V16C200,7.2,192.8,0,184,0z\"></path></svg></p>"
+  }
+];
+
 const gamesDataee= [
   {
     href: "tjps.html",
@@ -108,14 +121,34 @@ const gamesDataee= [
 
 const gamesDatalv= [
   {
-    href: "",
+    href: "tjpp4",
     image: "TeeKOGame",
-    name: "Tee K.O.",
+    name: "Kre K.O.",
     span: "PĀRTULKOTS",
     date: "~80%",
     new: "",
     view: ""
   }
+];
+
+const newgamesDataee= [
+  {}
+];
+
+const newgamesDatalv= [
+  {
+    href: "tjpp4",
+    image: "TeeKOGame",
+    name: "Kre K.O.",
+    span: "THE JACKBOX PARTY PACK 4",
+    date: "2022. gada 21. decembris",
+    new: "<p data-v-556a942f=\"\" class=\"new\">JAUNS</p>",
+    view: "<p data-v-556a942f=\"\" class=\"cta\">APLŪKOT ŠO SPĒLI <svg data-v-556a942f=\"\" class=\"external-icon\" viewBox=\"0 0 200 200\"><path d=\"M185,106c-8.3,0-15,6.7-15,15v44c0,2.8-2.2,5-5,5H35c-2.8,0-5-2.2-5-5V35c0-2.8,2.2-5,5-5h44c8.3,0,15-6.7,15-15S87.3,0,79,0H35C15.7,0,0,15.7,0,35v130c0,19.3,15.7,35,35,35h130c19.3,0,35-15.7,35-35v-44C200,112.7,193.3,106,185,106z\"></path><path d=\"M184,0h-54c-8.3,0-15,6.7-15,15s6.7,15,15,15h18.8L90.4,88.4c-5.9,5.9-5.9,15.4,0,21.2c2.9,2.9,6.8,4.4,10.6,4.4s7.7-1.5,10.6-4.4L170,51.2V70c0,8.3,6.7,15,15,15s15-6.7,15-15V16C200,7.2,192.8,0,184,0z\"></path></svg></p>"
+  }
+];
+
+const newgamesDatalt= [
+  {}
 ];
 
 const gamesDatalt= [
@@ -151,6 +184,24 @@ function games(text) {
       <div data-v-556a942f="" class="content">
         <p data-v-556a942f="" class="name">${text.name}</p>
         <p data-v-556a942f="" class="date">
+          <span data-v-556a942f="">${text.span}</span>${text.date}
+        </p>
+      </div>
+      ${text.new}
+    </a>
+  </div>
+  `;
+}
+
+function newgames(text) {
+  return `
+  <div data-v-556a942f="" data-v-2ba6adc4="" class="past-game home">
+    <a data-v-556a942f="" href="${text.href}" target="_blank" class="card">
+      <div data-v-556a942f="" class="image fallback ${text.image}"></div>
+      ${text.view}
+      <div data-v-556a942f="" class="content">
+        <p data-v-556a942f="" class="name2">${text.name}</p>
+        <p data-v-556a942f="" class="date2">
           <span data-v-556a942f="">${text.span}</span>${text.date}
         </p>
       </div>
@@ -231,15 +282,15 @@ const aboutusee= [
   {
     class: "class=\"ee ct\"",
     welcome: "",
-    jackboxbaltic: "",
-    aboutjg: "",
+    jackboxbaltic: "<strong>Jackbox Baltic</strong> on veebileht, kuhu me laeme üles mitteametlikud eesti-, läti- ja leedukeelsed tõlked Jackbox Games mängudele, tuntud kui Jackbox Party Pack'i loojad. Oleme alles oma tõlketeekonna alguses, seega võid meile appi tulla meie Discordi serveris!",
+    aboutjg: "Mis on Jackbox Games",
     aboutjgd: "",
     cons: "",
     ttcons: "",
     tecons: "",
     fwcons: "",
     gcons: "",
-    sthanks: "",
+    sthanks: "Suured tänud",
     cont: "",
     contd: "",
     ds: "DISCORDI SERVER",
@@ -253,15 +304,15 @@ const aboutuslv= [
     welcome: "",
     jackboxbaltic: "<strong>Jackbox Baltic</strong> ir vietne, kurā mēs augšupielādējam neoficiālus tulkojumus igauņu, latviešu un lietuviešu valodās Jackbox Games spēlēm (kas ir pazīstami kā Jackbox Party Pack radītāji). Mēs tikai sākam savu ceļu spēļu lokalizācijā, tāpēc tu vari mums palīdzēt, pievienojoties mūsu komandai Discord serverī!",
     aboutjg: "Par Jackbox Games",
-    aboutjgd: "",
-    cons: "",
-    ttcons: "",
-    tecons: "",
-    fwcons: "",
-    gcons: "",
-    sthanks: "",
-    cont: "",
-    contd: "",
+    aboutjgd: "<strong>Jackbox Games</strong> ir neliela spēļu ražošanas studija no Čikāgas (Ilinoisas štatā). Slavenākā franšīze - You Don't Know Jack - tika izlaista kopš 1995. gada un pat pastāvēja kā TV šova. Būt par daļu no Jellyvision, 2013. gadā tas atdalījās un tika nosaukts par Jackbox Games. Līdzās The Jackbox Party Pack franšīzei tiek izlaistas dažādas spēles, piemēram - Quiplash, Fibbage, Trivia Murder Party.",
+    cons: "Dalībnieki",
+    ttcons: "Teksta tulkotāji",
+    tecons: "Tehniskais redaktors",
+    fwcons: "Fonta darbinieks",
+    gcons: "Grafikas darbinieks",
+    sthanks: "Īpašs paldies",
+    cont: "Kontakts",
+    contd: "Ja tev ir kāds jautājums, ieteikums vai vienkārši vēlies aprunāties, vislabāk sazinies, izmantojot:",
     ds: "DISCORD SERVERIS",
     from: "no",
   }
@@ -316,7 +367,7 @@ function aboutus(text) {
               <p data-v-2ba6adc5=""><strong>${text.ttcons}</strong>: Alacris <span>(eesti)</span>, Dejelnieks <span>(latviešu)</span>, Vvv <span>(lietuvių)</span></p>
               <p data-v-2ba6adc5=""><strong>${text.tecons}</strong>: Dejelnieks <span>(eesti, latviešu, lietuvių)</span></p>
               <p data-v-2ba6adc5=""><strong>${text.fwcons}</strong>: Dejelnieks <span>(eesti, latviešu, lietuvių)</span></p></p>
-              <p data-v-2ba6adc5=""><strong>${text.gcons}</strong>: Trstnlkr2582 .D <span>(from Jackbox en español)</span></p>
+              <p data-v-2ba6adc5=""><strong>${text.gcons}</strong>: Trstnlkr2582 .D <span>(${text.from} Jackbox en español)</span></p>
               <p data-v-2ba6adc5=""><strong>${text.sthanks}</strong>: Trstnlkr2582 .D, Akira896 <span>(${text.from} Jackbox en español)</span>, Erizzle <span>(${text.from} Jackbox Translations DE)</p>
             </div>
           </div>
@@ -339,6 +390,95 @@ function aboutus(text) {
         </div>
       </div>
     </div>
+  </div>
+  `;
+}
+
+const tjppen= [
+  {
+    title: "You Don't Know Jack 2015",
+    status: "Not translated",
+    image: "ydkj2015",
+    description: "The smart-ass trivia show that started it all! Hundreds of all-new twisted trivia questions and everyone’s favorite host!"
+  },
+  {
+    title: "Drawful",
+    status: "Not translated",
+    image: "drawful",
+    description: "The first installment of the wildly popular drawing game allows you to draw bizarre doodles on your phone or tablet."
+  },
+  {
+    title: "Word Spud",
+    status: "Not translated",
+    image: "wordspud",
+    description: "Test your vocabulary chops in the racy-as-you-want-it-to-be fill-in-the-blank word game."
+  },
+  {
+    title: "Lie Swatter",
+    status: "Not translated",
+    image: "lieswatter",
+    description: "Need a game for a big group? Grab yourself or a crowd and play true-or-false with a timer."
+  },
+  {
+    title: "Fibbage XL",
+    status: "Not translated",
+    image: "fibbagexl",
+    description: "The hilarious bluffing game now has 50% more questions added to the original hit game, Fibbage."
+  }
+];
+
+function tjpp(text) {
+  return `
+  <div data-v-556a942f="" data-v-2ba6adc4="" class="past-game home">
+    <a data-v-556a942f="" class="card">
+      <div data-v-556a942f="" class="content">
+          <p data-v-556a942f="" class="name">${text.title}</p>
+          <p data-v-556a942f="" class="date">${text.status}</p>
+        </div>
+      <div data-v-556a942f="" class="image fallback ${text.image}"></div>
+      <div data-v-556a942f="" class="content">
+          <p data-v-556a942f="" class="text">${text.description}</p>
+      </div>
+    </a>
+  </div>
+  `;
+}
+
+const test= [
+  {}
+];
+
+const games2test= [
+  {
+    title: "Recently translated game(s)",
+  }
+];
+
+const games2ee= [
+  {
+    title: "",
+  }
+];
+
+const games2lv= [
+  {
+    title: "Nesen tulkota spēle",
+  }
+];
+
+const games2lt= [
+  {
+    title: "",
+  }
+];
+
+function games2(text) {
+  return `
+  <div data-v-2ba6adc4="" class="top-items">
+    <h3 data-v-2ba6adc4="">${text.title}</h3>
+  </div>
+  <div class="centerpls">
+    <div id="newgames"></div>
   </div>
   `;
 }
@@ -371,20 +511,11 @@ const langArr = {
     statust: 'Translated',
     statusdnt: 'Not translated',
     statusip: 'In process',
-    ydkj2015: 'You Don\'t Know Jack 2015',
-    ydkj2015d: 'The smart-ass trivia show that started it all! Hundreds of all-new twisted trivia questions and everyone’s favorite host!',
-    drawful: 'Drawful',
-    drawfuld: 'The first installment of the wildly popular drawing game allows you to draw bizarre doodles on your phone or tablet.',
-    wordspud: 'Word Spud',
-    wordspudd: 'Test your vocabulary chops in the racy-as-you-want-it-to-be fill-in-the-blank word game.',
-    lieswatter: 'Lie Swatter',
-    lieswatterd: 'Need a game for a big group? Grab yourself or a crowd and play true-or-false with a timer.',
-    fibbagexl: 'Fibbage XL',
-    fibbagexld: 'The hilarious bluffing game now has 50% more questions added to the original hit game, Fibbage.',
     screenblocker: 'Unfortunately, this screen scale isn\'t supported!',
     nttext: 'At the moment, we don\'t have a translation. But you can help join the translation team!',
     aboutus: 'about us',
     titleaboutus: 'About Us',
+    recentlytgtitle: 'Recently translated game(s)',
   },
   ee: {
     ds: 'DISCORDI SERVER',
@@ -431,6 +562,7 @@ const langArr = {
     nttext: 'Šobrīd mums nav tulkojuma. Bet tu vari palīdzēt pievienoties tulkošanas komandai!',
     aboutus: 'par mums',
     titleaboutus: 'Par mums',
+    recentlytgtitle: 'Nesen tulkota spēle',
   },
   lt: {
     ds: 'DISCORDO SERVERIS',
@@ -457,12 +589,14 @@ const langEe = document.querySelector('#lang-ee');
 const langLv = document.querySelector('#lang-lv');
 const langLt = document.querySelector('#lang-lt');
 const langTest = document.querySelector('#lang-test');
-  
+
 langEn.addEventListener('click', setLang.bind(null, 'en'));
 langLv.addEventListener('click', setLang.bind(null, 'lv'));
 langEe.addEventListener('click', setLang.bind(null, 'ee'));
 langLt.addEventListener('click', setLang.bind(null, 'lt'));
-langTest.addEventListener('click', setLang.bind(null, 'test'));
+if(langTest){
+  langTest.addEventListener('click', setLang.bind(null, 'test'));
+}
 
 function setLang(lang) {
   if (!langArr.hasOwnProperty(lang)) return;
@@ -486,6 +620,11 @@ function setLang(lang) {
           ${aboutusen.map(aboutus).join("")}
         `;
       }
+      if(document.getElementById("tjpp")){
+        document.getElementById("tjpp").innerHTML = `
+          ${tjppen.map(tjpp).join("")}
+        `;
+      }
     }
     if(lang=='ee') {
       console.log('Eesti');
@@ -497,6 +636,11 @@ function setLang(lang) {
       if(document.getElementById("aboutus")){
         document.getElementById("aboutus").innerHTML = `
           ${aboutusee.map(aboutus).join("")}
+        `;
+      }
+      if(document.getElementById("games2")){
+        document.getElementById("games2").innerHTML = `
+          ${games2ee.map(games2).join("")}
         `;
       }
     }
@@ -512,6 +656,16 @@ function setLang(lang) {
           ${aboutuslv.map(aboutus).join("")}
         `;
       }
+      if(document.getElementById("games2")){
+        document.getElementById("games2").innerHTML = `
+          ${games2lv.map(games2).join("")}
+        `;
+      }
+      if(document.getElementById("newgames")){
+        document.getElementById("newgames").innerHTML = `
+          ${newgamesDatalv.map(newgames).join("")}
+        `;
+      }
     }
     if(lang=='lt') { 
       console.log('Lietuvių');
@@ -525,6 +679,11 @@ function setLang(lang) {
           ${aboutuslt.map(aboutus).join("")}
         `;
       }
+      if(document.getElementById("games2")){
+        document.getElementById("games2").innerHTML = `
+          ${games2lt.map(games2).join("")}
+        `;
+      }
     }
     if(lang=='test') {
       console.log('Test');
@@ -536,6 +695,16 @@ function setLang(lang) {
       if(document.getElementById("aboutus")){
         document.getElementById("aboutus").innerHTML = `
           ${aboutusen.map(aboutus).join("")}
+        `;
+      }
+      if(document.getElementById("games2")){
+        document.getElementById("games2").innerHTML = `
+          ${games2test.map(games2).join("")}
+        `;
+      }
+      if(document.getElementById("newgames")){
+        document.getElementById("newgames").innerHTML = `
+          ${newgamesDatatest.map(newgames).join("")}
         `;
       }
     }
--- assets/js/siteversion.js
+++ assets/js/siteversion.js
@@ -1,6 +1,6 @@
 function siteversion() {
 	var element = document.getElementById("sv");
-	element.innerHTML = "221218-3";
+	element.innerHTML = "221221";
 }
 
 siteversion()
